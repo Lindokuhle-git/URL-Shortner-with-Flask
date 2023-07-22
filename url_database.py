98e6ed42dd83d53ea036a5ea79a99ce309d8bd7f
Lindokuhle-git <lindokuhlerajuili@gmail.com>
2023-07-22 16:41:27 +0200

    refactored the url_database.py file to be non redundant

--- url_database.py
+++ url_database.py
@@ -3,41 +3,21 @@ import os
 
 database_path = 'urlshortner.db'  # Replace with the desired database file path
 
-# # Check if the database file exists
-# if os.path.exists(database_path):
-#     # Open the existing database
-#     conn = sqlite3.connect(database_path)
-# else:
-#     # Create a new database and connect to it
-#     conn = sqlite3.connect(database_path)
+# Check if the database file exists
+if os.path.exists(database_path):
+    # Open the existing database
+    conn = sqlite3.connect(database_path)
+else:
+    # Create a new database and connect to it
+    conn = sqlite3.connect(database_path)
 
-# # Create a cursor object to execute SQL commands
-# cursor = conn.cursor()
-
-# try:
-#     # Create the required tables if they don't exist
-#     
-
-
-#     # Commit the changes
-#     conn.commit()
-
-#     # Perform other operations with the database as needed
-#     # ...
-    
-# except sqlite3.Error as e:
-#     print(f"An error occurred: {e}")
-
-# finally:
-#     # Close the database connection
-#     conn.close()
-
+# Create a cursor object to execute SQL commands
+cursor = conn.cursor()
 
 
 def add_url(original_url, shortened_url):
     try:
-        conn = sqlite3.connect(database_path)
-        cursor = conn.cursor()
+        
         
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS shortenedURL (
@@ -66,8 +46,7 @@ def delete_url():
 
 
 def show_stored_url():
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
+    
     cursor.execute('SELECT * FROM shortenedURL')
     data = cursor.fetchall()
 
@@ -78,10 +57,7 @@ def show_stored_url():
 
 
 def fetch_mapped_url(short_code):
-    # Connect to the SQLite database
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
-
+    
     # Retrieve the original URL from the database based on the short code
     cursor.execute('SELECT original_url FROM shortenedURL WHERE shortened_url = ?', (short_code,))
     result = cursor.fetchone()
